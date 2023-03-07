9e24cd414e96348a5f333516aa74938db8a6277b
Rafael de Bem <debemrafael@gmail.com>
2023-03-07 09:19:30 -0300

    Create INDEXes

--- database/initdb.d/setup.sql
+++ database/initdb.d/setup.sql
@@ -2,5 +2,6 @@ CREATE TABLE data (
     id int(11) not null primary key auto_increment,
     url varchar(255) not null,
     title varchar(100) not null,
-    date_added datetime not null
+    date_added datetime not null,
+    INDEX (url, title, date_added)
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
