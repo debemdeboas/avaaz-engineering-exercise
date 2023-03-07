b5d5282eb4a91978dd0d35002c99d4612b03380e
Rafael de Bem <debemrafael@gmail.com>
2023-03-07 12:27:25 -0300

    Add crude HTML pages

--- app/server.py
+++ app/server.py
@@ -1,12 +1,14 @@
 
-from flask import Flask, make_response, request
+from json2html import json2html
+from flask import Flask, make_response, render_template, request
+from flask_bootstrap import Bootstrap
 
 from db_controller import *
 from models.data import DataModel, SearchQueryDateFilter
 from utils import parse_date
 
-
 app = Flask(__name__)
+Bootstrap(app)
 
 
 def parse_search_query(title: str | None, url: str | None, date: str | None) -> list[DataModel] | tuple[str, int]:
@@ -17,7 +19,7 @@ def parse_search_query(title: str | None, url: str | None, date: str | None) -> 
     elif date:
         try:
             filter_type = SearchQueryDateFilter(
-                request.args.get('filter_by', 'after').lower().strip()
+                request.args.get('radio-datefilter', 'after').lower().strip()
             )
             start_date = parse_date(date)
             match filter_type:
@@ -26,7 +28,7 @@ def parse_search_query(title: str | None, url: str | None, date: str | None) -> 
                 case SearchQueryDateFilter.after:
                     return search_by_date_after(start_date)
                 case SearchQueryDateFilter.between:
-                    end_date = request.args.get('end_date')
+                    end_date = request.args.get('datetime-picker-enddate')
                     if not end_date:
                         return 'No end date specified, but filter "between" was selected!', 400
 
@@ -34,14 +36,14 @@ def parse_search_query(title: str | None, url: str | None, date: str | None) -> 
                     return search_by_date_between(start_date, end_date)
         except ValueError:
             return 'Invalid date or search filter', 400
-    return 'Unspecified error', 500
+    return 'Invalid input', 500
 
 
 @app.get('/search')
 def search_data():
-    title = request.args.get('title')
-    url = request.args.get('url')
-    date = request.args.get('date')
+    title = request.args.get('db-search-title')
+    url = request.args.get('db-search-url')
+    date = request.args.get('datetime-picker-date')
 
     if res := parse_search_query(title, url, date):
         # The result of `parse_search_query` will be a tuple[str, int]
@@ -49,8 +51,8 @@ def search_data():
         # and the int value will be the HTTP return code.
         if isinstance(res, tuple):
             return make_response(*res)
-        item_list = [entry.to_json() for entry in res]
-        return make_response({'items': item_list}, 200)
+        item_list = {'items': [entry.to_json() for entry in res]}
+        return make_response(json2html.convert(json=item_list), 200) #type: ignore
     return make_response('Record(s) not found', 404)
 
 
@@ -60,12 +62,14 @@ def ingest_json():
     if (not js_data) or ('items' not in js_data):
         return 'Invalid JSON', 400
 
+    ingest_json_data(js_data)
+
     return '', 204
 
 
-@app.route('/'):
+@app.route('/')
 def homepage():
-    
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
